7a48c42d67ec305dbd248d7e169dcec4a5304aa1
ztianming <1003160664@qq.com>
2018-11-12 19:17:36 +0800

    Batch Command

---
+++ 批量加后缀.bat
@@ -0,0 +1,46 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+:这里可以指定添加的后缀名
+set hz=jpg
+set batname=%0
+echo =================================================================
+echo                         输入 HY 为还原后缀 
+echo                        否则自动加上jpg后缀
+echo                           关闭直接X掉就好
+echo =================================================================
+set /p a=你输入的是：
+if !a!==HY (
+echo !a! 文件正在还原
+goto :return
+)
+::默认直接在名称后追加后缀
+FOR /R %~dp0 %%i IN (*) DO (
+if NOT "%%~fi"==%batname% (
+ECHO "%%~nxi" TO "%%~nxi.!hz!"
+ren "%%~nxi" "%%~nxi.!hz!"
+)
+)
+pause
+EXIT
+
+:return
+call :getlen %hz%
+FOR /R %~dp0 %%i IN (*) DO (
+if NOT "%%~fi"==%batname% (
+SET KK=%%~nxi
+SET RETUN=!KK:~0,-%getlennum%!
+ECHO 正在修改文件 %%~nxi TO !RETUN!
+ren "%%~nxi" "!RETUN!"
+)
+)
+pause
+exit
+
+:getlen
+set getlennum=
+set getlenstr=%1
+:len
+set getlenstr=%getlenstr:~0,-1%
+set /a getlennum = %getlennum% + 1 
+if defined getlenstr goto len
+GOTO :EOF
---
+++ 统计txt文本行数.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+(for /f "delims=" %%a in ('dir /a-d /b *.txt') do (
+set n=0
+for /f "delims=" %%b in ('type "%%a"') do set /a n+=1
+if "%%a" neq "统计.txt" echo,%%a --- !n!
+))>"统计各txt行数.txt"
